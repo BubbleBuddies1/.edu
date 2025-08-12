c640e268972a8c162a300776b1608b8ff52e118f
BubbleBuddies1 <Bubblebuddiesmath@gmail.com>
2025-08-12 18:44:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('requestBtn').addEventListener('click', function() {
+    alert('Thanks for your interest! Please send your topic suggestion to BubbleBuddiesMath@gmail.com.');
+});
+console.log("Bubble Buddies site loaded!");
